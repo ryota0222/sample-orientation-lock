30178b0a0465b06a93385ca2f9540939d6dba40e
ryota0222 <ryota0222@users.noreply.github.com>
2023-04-20 03:03:38 +0000

    deploy: 371f2a3db9c0c7fa1145f07740cc454d8bdb4a3b

---
+++ _next/static/nggP7DVPehKU9zq0jodii/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/92ff1f22dd9892a3.css","static/chunks/pages/index-01c759be44e9e117.js"],"/_error":["static/chunks/pages/_error-f170ec85fda4fc04.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
