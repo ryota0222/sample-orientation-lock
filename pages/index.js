79a874ba6316b9d69dc7f187e933c0bc6ad08fde
ryota0222 <45546517+ryota0222@users.noreply.github.com>
2023-04-21 12:41:52 +0900

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
           setOrientation(value);
         })
         .catch((err) => {
-          console.log(err);
           setOrientation(undefined);
         });
     },
