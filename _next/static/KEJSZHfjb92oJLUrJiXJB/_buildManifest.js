1a9b12cfb58c21023d8c6a9df26eda1d40e9e166
ryota0222 <ryota0222@users.noreply.github.com>
2023-04-21 03:42:57 +0000

    deploy: 79a874ba6316b9d69dc7f187e933c0bc6ad08fde

---
+++ _next/static/KEJSZHfjb92oJLUrJiXJB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8707a95168f2c8f2.css","static/chunks/pages/index-9f39cc551c62e693.js"],"/_error":["static/chunks/pages/_error-f170ec85fda4fc04.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
