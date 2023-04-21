6ebe4072bab0adb1314e70de4db23dedb7abb206
ryota0222 <ryota0222@users.noreply.github.com>
2023-04-21 05:10:15 +0000

    deploy: cf4d67bb2c22ca307829a8f0d6be08076929bb07

--- _next/static/VJXl90U1J1jIhKP-Kw2si/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8707a95168f2c8f2.css","static/chunks/pages/index-f8a41f65297c729c.js"],"/_error":["static/chunks/pages/_error-f170ec85fda4fc04.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/paZdn3YCzurUiVywxKjoO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8707a95168f2c8f2.css","static/chunks/pages/index-716de91fc6d2cd01.js"],"/_error":["static/chunks/pages/_error-f170ec85fda4fc04.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
